58a31120af21122318e20a849e9cd6a377fce116
limematheuz <mribeiro17.info@gmail.com>
2024-04-11 14:05:33 +0200

    add video at homepage

--- src/js/index.js
+++ src/js/index.js
@@ -41,8 +41,12 @@ function createLandingSection() {
   let seeCatalogContainer = document.createElement("div");
   seeCatalogContainer.id = "card";
 
-  let backgroundImage = document.createElement("img");
-  backgroundImage.src = "Rectangle 5 (1).png";
+  let backgroundImage = document.createElement("video");
+  backgroundImage.src = "../img/playIntro.mp4";
+  backgroundImage.autoplay = true;
+  backgroundImage.muted = true;
+  backgroundImage.loop = true;
+  // backgroundImage.src = "../img/homeImage.jpg";
 
   let catalogButton = document.createElement("button");
   catalogButton.textContent = "SEE CATALOG";
